3101455178551b46dd53c63bd6e1fc9363339d6c
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-20 14:37:43 -0500

    Create Child Compoenent

---
+++ src/components/BookItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>{{ book.title }}:{{ book.author }}</li>
+  <li v-for="book in books" :key="book.title">{{ book.title }}:{{ book.author }}</li>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  data() {
+    return {
+      props: ["book"],
+    };
+  },
+};
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
