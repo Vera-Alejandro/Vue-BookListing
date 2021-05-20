4b7ae2df3551e4ce58035ba98448b0a024cea567
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-20 13:53:28 -0500

    Book List vue create

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+    
+<script>
+export default {
+  name: "BookList",
+  title: "All Books",
+  data() {
+    return title;
+  },
+};
+</script>
+    
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
